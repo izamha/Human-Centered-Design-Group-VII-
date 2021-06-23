394d55816820d9a5dffec3a91e22b4303fa5d18a
guilfoyle <glcode40@gmail.com>
2021-06-23 17:38:04 +0200

    Fourth Changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-  $('#action_menu_btn').click(function () {
-    $('.action_menu').toggle();
-  });
+    $('#action_menu_btn').click(function () {
+        $('.action_menu').toggle();
+    });
+    $('.settings').click(function () {
+        $('.show-settings').toggle();
+    })
 });
+
+// Hide the menus upon clicking outside
+// $(document).on('click', function (e) {
+//     if ($(e.target).closest(".show-settings").length === 0) {
+//         $(".show-settings").hide();
+//     }
+// });
